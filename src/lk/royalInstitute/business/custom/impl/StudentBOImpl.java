7e4ffe6b02cfa9d427ec38fc5f40759f64b0dcfb
JanithRavinduRashmika <rashmikajanithravindu@gmail.com>
2021-02-06 09:23:00 +0530

    update StudentBOImpl

--- src/lk/royalInstitute/business/custom/impl/StudentBOImpl.java
+++ src/lk/royalInstitute/business/custom/impl/StudentBOImpl.java
@@ -8,6 +8,7 @@ import lk.royalInstitute.dao.custom.StudentDAO;
 import lk.royalInstitute.dto.StudentDTO;
 import lk.royalInstitute.entity.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentBOImpl implements StudentBO {
@@ -25,21 +26,42 @@ public class StudentBOImpl implements StudentBO {
 
     @Override
     public boolean deleteStudent(String id) throws Exception {
-        return false;
+        return studentDAO.delete(id);
     }
 
     @Override
     public boolean updateStudent(StudentDTO studentDTO) throws Exception {
-        return false;
+        return studentDAO.update(new Student(studentDTO.getId(),
+                studentDTO.getStudentName(),
+                studentDTO.getAddress(),
+                studentDTO.getContact(),
+                studentDTO.getDate(),
+                studentDTO.getGender()));
     }
 
     @Override
     public StudentDTO getStudent(String id) throws Exception {
-        return null;
+        Student student = studentDAO.get(id);
+        return new StudentDTO(student.getId(),
+                student.getStudentName(),
+                student.getAddress(),
+                student.getContact(),
+                student.getDate(),
+                student.getGender());
     }
 
     @Override
     public List<StudentDTO> getAllStudents() throws Exception {
-        return null;
+        List<StudentDTO> studentDTOList = new ArrayList<>();
+        List<Student> studentList = studentDAO.getAll();
+        for (Student student : studentList) {
+            studentDTOList.add(new StudentDTO(student.getId(),
+                    student.getStudentName(),
+                    student.getAddress(),
+                    student.getContact(),
+                    student.getDate(),
+                    student.getGender()));
+        }
+        return studentDTOList;
     }
 }
